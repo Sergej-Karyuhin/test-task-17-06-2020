c769ffbe26a207d385fab904190dc7fee98a8a65
karyuhin.sergej@gmail.com <karyuhin.sergej@gmail.com>
2020-06-18 02:24:12 +0300

    feat: add calendar and swap img by date

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Calendar from 'react-calendar';
+import './App.css';
+import 'react-calendar/dist/Calendar.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { data: [] };
+    this.state = {
+      data: [],
+      date: new Date()
+    };
   }
 
   componentDidMount() {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=PNNAo56BqMP30bfAhhdgwO8zjgclbjq34JAwtYEA&date=2020-03-16`)
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=PNNAo56BqMP30bfAhhdgwO8zjgclbjq34JAwtYEA`)
       .then(res => res.json())
       .then(json => this.setState({ data: json }));
   }
 
+  changeImage = (date) => {
+    let y = (date.getFullYear()).toString();
+    let m = (date.getMonth() + 1).toString();
+    let d = (date.getDate()).toString();
+    m = (m.length) === 1 ? '0' + m : m;
+    d = (d.length) === 1 ? '0' + d : d;
+    let formatted_date = y + '-' + m + '-' + d;
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=PNNAo56BqMP30bfAhhdgwO8zjgclbjq34JAwtYEA&date=${formatted_date}`)
+      .then(res => res.json())
+      .then(json => this.setState({ data: json }));
+  }
 
+  onChange = date => {
+    this.changeImage(date);
+  };
 
   render() {
-    console.log(this.state.data);
     return (
-      <div className="App">
-        <h1>Rnq</h1>
-        <img src={this.state.data.url} />
+      <div className="app">
+        <h1>Date must be between Jun 16, 1995 and Jun 17, 2020.</h1>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
+        <img src={this.state.data.url} width="512px"/>
       </div>
     );
   }
