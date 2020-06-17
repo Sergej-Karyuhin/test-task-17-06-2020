5d04e7f34d814e878f1175af5172afa7b3f48399
Karyuhin Sergej <karyuhin.sergej@gmail.com>
2020-06-17 16:01:53 +0300

    feat: start to do project, add yesterday img

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Rnq</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { data: [] };
+  }
+
+  componentDidMount() {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=PNNAo56BqMP30bfAhhdgwO8zjgclbjq34JAwtYEA&date=2020-03-16`)
+      .then(res => res.json())
+      .then(json => this.setState({ data: json }));
+  }
+
+
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <div className="App">
+        <h1>Rnq</h1>
+        <img src={this.state.data.url} />
+      </div>
+    );
+  }
 }
 
 export default App;
